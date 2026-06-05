5cb9fc113e9822dd2c78cb5fcee1cf584dc1bbe8
Vvnu <thakurvinu00@gmail.com>
2026-06-05 10:28:29 +0530

    metafile tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,12 @@
+import type { Metadata } from "next";
 import "./globals.css";
 import { AuthProvider } from "@/context/AuthContext";
+
+export const metadata: Metadata = {
+  verification: {
+    google: "qW26ogIu1-9fHtgyxDe1UoMfGE8li_DYQ9i_ct-Z2co",
+  },
+};
 
 export default function RootLayout({
   children,
